532e6ea06d124a3ac7d86ecca92b359e1feba43a
massinissa-boutrig <173133@supinfo.com>
2015-08-28 13:51:21 +0200

    Recuperation d'adresse ip publique :: OK :: Comparaison avec l'ancienne :: OK :: envoi de mail :: OK ::

---
+++ getmyip.py
@@ -0,0 +1,75 @@
+__author__ = 'chaoswow'
+
+import ipgetter
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class IPman:
+    MYIP = "/home/chaoswow/my_ip"
+    # WEBIP = "/home/chaoswow/web_ip"
+
+    def getMyIp(self):
+        ip = ipgetter.myip()
+        print(ip)
+        return ip
+
+    def readMyFile(self, path):
+        try:
+            with open(path, 'r') as f:
+                r = f.read()
+                print(r)
+                return r
+        except IOError as err:
+            print("Erreur de type Input/Output dans la methode readMyFile \n", err)
+
+    def writeMyFile(self, path, webip):
+        try:
+            with open(path, 'w') as f:
+                f.write(webip)
+        except IOError:
+            print("Erreur de type Input/Output dans la methode writeMyFile")
+
+    def sendMail(self, newIP):
+        fromaddr = "raspberrypython3@gmail.com"
+        toaddr = "raspberrypython3@gmail.com"
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = "IP address has been changed"
+
+        partialbody = "Mail automatique : \n"
+        body = partialbody + newIP
+        msg.attach(MIMEText(body, 'plain'))
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, "Raspipython3")
+        text = msg.as_string()
+        try:
+            server.sendmail(fromaddr, toaddr, text)
+        except smtplib.SMTPException as err:
+            print(err)
+        except:
+            print("Erreur d'envoi")
+
+        server.quit()
+
+
+webIp = IPman().getMyIp()  # Get IP from web
+r = IPman().readMyFile(IPman.MYIP)  # Read IP in file
+
+""" On compare l'adresse ip obtenue et celle qu'on a inscrit """
+if r == webIp:
+    print("No diff")
+if r != webIp:
+    print("There are diff !  /!\\")
+    IPman().sendMail(webIp)
+    IPman().writeMyFile(IPman.MYIP, webIp)
+print(type(r))
+
+
+
+# IPman().writeMyFile(IPman.MYIP, webIp)
